fbc0d21af5042ce46a87f770ec5fd1080bb85107
yar0-slav <hornster1@gmail.com>
2023-03-27 10:35:07 +0200

    apollo prod url

--- components/api/apolloClient.ts
+++ components/api/apolloClient.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  // uri: "https://strapi-production-9d17.up.railway.app/graphql",
-  uri: "http://localhost:1337/graphql",
+  uri: "https://strapi-production-9d17.up.railway.app/graphql",
   cache: new InMemoryCache(),
 });
